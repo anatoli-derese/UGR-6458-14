7e5f810318a656bbf8ccf73fbe7ea53e68b1ab77
Anatoli Derese <anatoli.derese@aait.edu.et>
2023-12-16 21:58:52 +0300

    document structures

---
+++ src/js/professional.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementById('hamburgerToggle');
+    const navbar = document.getElementById('navbar');
+    hamburger.addEventListener('click', onHamburgerClick );
+    
+     function onHamburgerClick() {
+        if (navbar.classList.contains('open')) {
+            navbar.classList.remove('open');
+            return;
+        }
+        navbar.classList.add('open');
+        
+    }  
