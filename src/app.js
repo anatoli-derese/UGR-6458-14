59c5839c7e0c83904595276f5626fae354332e37
Anatoli Derese <anatoli.derese@aait.edu.et>
2023-12-22 13:43:42 +0300

    home page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burgerIcon = document.getElementById('burger');
-    const navBarMenu = document.getElementById("nav-links");
+document.getElementById('mobileMenuButton').addEventListener('click', function() {
+    document.getElementById('mobileMenu').classList.toggle('hidden');
+  });
 
-    burgerIcon.addEventListener('click', () => {
-        navBarMenu.classList.toggle("hidden");
-        burgerIcon.classList.toggle("hidden");
-    });
-});
+  function Menu(e){
+    let list = document.querySelector('ul');
+    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+  }
